fc33085bc300a12e5fed4e31f8f65279df22a5ac
erikyoung <erik.young@rmit.edu.vn>
2017-07-02 00:02:23 +0700

    added login functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215104433) do
+ActiveRecord::Schema.define(version: 20170701165213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,12 @@ ActiveRecord::Schema.define(version: 20151215104433) do
     t.text     "extended_html_description"
     t.integer  "category_id"
     t.string   "name"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
+    t.boolean  "publish",                   default: false
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_events_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
     t.index ["venue_id"], name: "index_events_on_venue_id", using: :btree
   end
 
@@ -51,6 +54,34 @@ ActiveRecord::Schema.define(version: 20151215104433) do
     t.index ["event_id"], name: "index_ticket_types_on_event_id", using: :btree
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.integer  "ticket_type_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.integer  "count"
+    t.index ["ticket_type_id"], name: "index_tickets_on_ticket_type_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
   create_table "venues", force: :cascade do |t|
     t.string   "name"
     t.string   "full_address"
@@ -63,5 +94,6 @@ ActiveRecord::Schema.define(version: 20151215104433) do
   add_foreign_key "events", "categories"
   add_foreign_key "events", "venues"
   add_foreign_key "ticket_types", "events"
+  add_foreign_key "tickets", "ticket_types"
   add_foreign_key "venues", "regions"
 end
