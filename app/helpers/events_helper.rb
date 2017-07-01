aed8267cd5a00c57ab60f081695ece8724d6a2f9
erikyoung <erik.young@rmit.edu.vn>
2017-07-02 00:33:41 +0700

    can create event

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,12 @@ module EventsHelper
   def header_background_image_url
     image_path('color-me-run.jpg')
   end
+
+   def get_hash_venue
+    Hash[Venue.all.collect { |venue| [venue.name, venue.id]}]
+  end
+
+  def get_hash_category
+    Hash[Category.all.collect { |category| [category.name, category.id]}]
+  end
 end
