5ce4f29150b9541f65ae92b3230fed31eb6d35c5
erikyoung <erik.young@rmit.edu.vn>
2017-07-03 02:41:39 +0700

    ticket relation with event

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,20 @@
 class Ticket < ApplicationRecord
+
+  belongs_to :event
+  belongs_to :ticket_type
+  # belongs_to :User, => class_name: "User"
+
+  validate :ticket_quantity
+  validates :quantity, numericality: {greater_than: 0, only_integer: true, less_than: 11}
+
+  private
+  def ticket_quantity
+    # TODO this is only correct for a specific user
+    # TODO count total number of avaliable tickets for this ticket_type
+
+    if ticket_type && quantity && quantity > ticket_type.remaining_quantity
+      errors.add(:quantity, "can't be greater than #{ticket_type.remaining_quantity} remaining tickets")
+    end
+  end
 end
+
